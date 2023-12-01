d90acfd7e4cdd91d60e71988b29284aee97ad587
reyanshcody <137416228+reyanshcody@users.noreply.github.com>
2023-12-01 17:37:40 +0530

    final-test

--- main.py
+++ main.py
@@ -5,17 +5,61 @@ op: number = 0
 o: number = 0
 
 def on_button_pressed_a():
-    global cmd, num1
+    global cmd, num1, num2
     cmd = "A"
     if cmd == 'A':
         num1 += 1
+        basic.show_number(num1)
     elif cmd == "A2":
-        pass
+        num2 += 1
+        basic.show_number(num2)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    global cmd
+    global cmd, o
     cmd = "AB"
     if cmd == "AB":
-        pass
+        while o < 4:
+            o += 1
+            if o == 1:
+                basic.show_leds("""
+                    . . # . .
+                    . . # . .
+                    # # # # #
+                    . . # . .
+                    . . # . .
+                    """)
+            if o == 2:
+                basic.show_leds("""
+                    . . . . .
+                    . . . . .
+                    # # # # #
+                    . . . . .
+                    . . . . .
+                    """)
+            if o == 3:
+                basic.show_leds("""
+                    # . . . #
+                    . # . # .
+                    . . # . .
+                    . # . # .
+                    # . . . #
+                    """)
+            if o == 4:
+                basic.show_leds("""
+                    . . # . .
+                    . . . . .
+                    # # # # #
+                    . . . . .
+                    . . # . .
+                    """)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global cmd
+    # pass
+    if cmd == "A":
+        cmd = "A2"
+    elif cmd = "A2":
+        pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
--- main.ts
+++ main.ts
@@ -4,17 +4,87 @@ let cmd:string = null;
 let op:number = 0;
 let o:number = 0;
 input.onButtonPressed(Button.A, function() {
-    cmd="A"
+    cmd="A";
     if(cmd=='A'){
-        num1++
+        num1++;
+        basic.showNumber(num1);
     }
     else if(cmd=="A2"){
-
+        num2++;
+        basic.showNumber(num2);
     }
 })
 input.onButtonPressed(Button.AB,function(){
     cmd ="AB";
     if(cmd=="AB"){
-        
+        while(o<4){
+            op++;
+            if(op==1){
+                
+                basic.showLeds(`
+                . . # . .
+                . . # . .
+                # # # # #
+                . . # . .
+                . . # . .
+                `);
+            }
+            if (op == 2) {
+
+                basic.showLeds(`
+                . . . . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . . . .
+                `)
+            }
+            if (op == 3) {
+
+                basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+                `)
+            }
+            if (op == 4) {
+
+                basic.showLeds(`
+                . . # . .
+                . . . . .
+                # # # # #
+                . . . . .
+                . . # . .
+                `)
+            }
+        }
     }
 });
+input.onButtonPressed(Button.B,function(){
+    if(cmd=="A"){
+        cmd="A2";
+    }
+    else if(cmd="A2"){
+        cmd="B";
+    }
+    
+    basic.showString(cmd);
+    if(cmd=="B"){
+        if(op==1){
+            o=num1+num2;
+        }
+        if (op == 2) {
+            o = num1 - num2;
+        }
+        if (op == 3) {
+            o = num1 * num2;
+        }
+        if (op == 4) {
+            o = num1 / num2;
+        }
+        basic.showNumber(o);
+    }
+    
+})
